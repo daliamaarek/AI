300baab7313a46d8e191dc12ec9e33ba013e0b6d
ahmad-diaa <ahmaddiaa93@gmail.com>
2016-09-30 15:33:52 +0300

    Prim seems to work

--- Cell.py
+++ Cell.py
@@ -1,9 +1,10 @@
 class Cell:
 	
-	def __init__(self,walls, isPokemon, row, column):
-		self.walls = walls
+	def __init__(self, isPokemon, row, column):
+		self.walls = ["UP","DOWN","LEFT","RIGHT"]
 		self.isPokemon = isPokemon
 		self.row = row
 		self.column = column
-
-
+	def __init__(self):
+		self.walls = ["UP","DOWN","LEFT","RIGHT"]
+		self.isPokemon = False
--- Maze.py
+++ Maze.py
@@ -1,4 +1,5 @@
 from random import randint
+from Cell import Cell
 
 class Maze:
 	
@@ -7,56 +8,53 @@ class Maze:
 		self.rows = rows
 		self.columns = columns
 		# generates a 2D rows x columns matrix having 0 in each cell 
-		self.visited_cells = [[0 for x in range(columns)] for y in range(rows)]
+		self.visited = [[0 for x in range(columns)] for y in range(rows)]
 
 		# for i in range(0,rows):
 		# 	for j in range(0,columns):
 		# 		print self.visited_cells[i][j]
 
 
-	def updateNew(self):
-		if(curr_wall[2] == "DOWN"):
-			self.field[curr_wall[0]][curr_wall[1]].walls.remove("DOWN")
-			self.field[new_row][new_column].walls.remove("UP")
-		elif(curr_wall[2] == "UP"):
-			self.field[curr_wall[0]][curr_wall[1]].walls.remove("UP")
-			self.field[new_row][new_column].walls.remove("DOWN")
-		elif(curr_wall[3] == "LEFT"):
-			self.field[curr_wall[0]][curr_wall[1]].walls.remove("LEFT")
-			self.field[new_row][new_column].walls.remove("RIGHT")
+	def update(self, start, end):
+		# print self.field[start[0]][start[1]].walls
+		if(start[2] == "DOWN"):
+			if("DOWN" in self.field[start[0]][start[1]].walls):
+				self.field[start[0]][start[1]].walls.remove("DOWN")
+			if("UP" in self.field[end[0]][end[1]].walls):
+				self.field[end[0]][end[1]].walls.remove("UP")
+		elif(start[2] == "UP"):
+			if("UP" in self.field[start[0]][start[1]].walls):
+				self.field[start[0]][start[1]].walls.remove("UP")
+			if("DOWN" in self.field[end[0]][end[1]].walls):
+				self.field[end[0]][end[1]].walls.remove("DOWN")
+		elif(start[2] == "LEFT"):
+			if("LEFT" in self.field[start[0]][start[1]].walls):
+				self.field[start[0]][start[1]].walls.remove("LEFT")
+			if("RIGHT" in self.field[end[0]][end[1]].walls):
+				self.field[end[0]][end[1]].walls.remove("RIGHT")
 		else:
-			self.field[curr_wall[0]][curr_wall[1]].walls.remove("RIGHT")
-			self.field[new_row][new_column].walls.remove("LEFT")
-
-	def updateOld(self):
-		if(curr_wall[2] == "DOWN"):
-			self.field[curr_wall[0]][curr_wall[1]].walls.remove("UP")
-			self.field[new_row][new_column].walls.remove("DOWN")
-		elif(curr_wall[2] == "UP"):
-			self.field[curr_wall[0]][curr_wall[1]].walls.remove("DOWN")
-			self.field[new_row][new_column].walls.remove("UP")
-		elif(curr_wall[3] == "LEFT"):
-			self.field[curr_wall[0]][curr_wall[1]].walls.remove("RIGHT")
-			self.field[new_row][new_column].walls.remove("LEFT")
-		else:
-			self.field[curr_wall[0]][curr_wall[1]].walls.remove("LEFT")
-			self.field[new_row][new_column].walls.remove("RIGHT")
+			if("RIGHT" in self.field[start[0]][start[1]].walls):
+				self.field[start[0]][start[1]].walls.remove("RIGHT")
+			if("LEFT" in self.field[end[0]][end[1]].walls):
+				self.field[end[0]][end[1]].walls.remove("LEFT")
 
 	# Randomized Prim's algorithm
 	def genMaze(self):
-		start_row = randint(0,self.rows)
-		start_column = randint(0,self.columns)
+		start_row = randint(0,self.rows - 1)
+		start_column = randint(0,self.columns - 1)
 		all_walls = []
-		curr_walls = self.field[start_row][start_column].walls
-		for i in range(0,len(cur_walls)):
+
+		curr_walls = self.field[start_row][start_column].walls[:]
+		for i in range(0,len(curr_walls)):
 			curr_walls[i] = (start_row, start_column, curr_walls[i])
-		self.visited_cells[start_row][start_column] = 1
+		self.visited[start_row][start_column] = 1
 		all_walls = all_walls + curr_walls
 
 		while (len(all_walls) != 0):
 			x = randint(0,len(all_walls) - 1)
 			curr_wall = all_walls[x]
-
+			print len(all_walls)
+			all_walls.remove(curr_wall)
 			# checks for border walls
 			if((curr_wall[0] == 0 and curr_wall[2] == "UP") or (curr_wall[1] == 0 and curr_wall[2] == "LEFT")
 				or (curr_wall[0] == self.rows - 1 and curr_wall[2] == "DOWN") or (curr_wall[1] == self.columns - 1 and curr_wall[2] == "RIGHT")):
@@ -65,31 +63,31 @@ class Maze:
 			new_column = curr_wall[1]
 
 			if(curr_wall[2] == "DOWN"):
-				new_row -= 1
+				new_row += 1
 			elif(curr_wall[2] == "UP"):
-				new_row += 1 
-			elif(curr_wall[3] == "LEFT"):
+				new_row -= 1 
+			elif(curr_wall[2] == "LEFT"):
 				new_column -= 1
 			else:
 				new_column += 1
 
-			all_walls.remove(curr_wall)
-
+			
+			# print str(curr_wall[0]) + " " + str(curr_wall[1])
+			# print str(new_row) + " " + str(new_column)
 			if(self.visited[curr_wall[0]][curr_wall[1]] == 1 and self.visited[new_row][new_column] == 0):
-				updateNew()
+				self.update(curr_wall,(new_row,new_column))
 				self.visited[new_row][new_column] = 1
-
-				curr_walls = self.field[new_row][new_column].walls
+				curr_walls = self.field[new_row][new_column].walls[:]
 				start_row = self.field[new_row][new_column].row
-				start_row = self.field[new_row][new_column].column
+				start_column = self.field[new_row][new_column].column
 				for i in range(0,len(curr_walls)):
 					curr_walls[i] = (start_row, start_column, curr_walls[i])
 				all_walls = all_walls + curr_walls
 
 			if(self.visited[curr_wall[0]][curr_wall[1]] == 0 and self.visited[new_row][new_column] == 1):
-				updateOld()
+				self.update(curr_wall,(new_row,new_column))
 				self.visited[curr_wall[0]][curr_wall[1]] = 1
-				curr_walls = self.field[curr_wall[0]][curr_wall[1]].walls
+				curr_walls = self.field[curr_wall[0]][curr_wall[1]].walls[:]
 				start_row = self.field[curr_wall[0]][curr_wall[1]].row
 				start_column = self.field[curr_wall[0]][curr_wall[1]].column
 				for i in range(0,len(curr_walls)):
@@ -98,8 +96,26 @@ class Maze:
 
 			
 
+rows = 4
+columns = 3
+field = [[Cell() for j in range(columns)] for i in range(rows)]
+for i in range(0,4):
+	for j in range(0,3):
+		field[i][j].row = i
+		field[i][j].column = j
 
-			#print curr_wall
+maze = Maze(field,rows,columns)
+maze.genMaze()
+for i in range(0,4):
+	for j in range(0,3):
+		print field[i][j].walls
+		# print  str(field[i][j].isPokemon)
+		# print field[i][j].row
+		# print field[i][j].column
+
+
+
+
 
 
 
