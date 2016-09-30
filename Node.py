5c0f6450abfaf7231441155970f42d96983ec785
Dalia Maarek <daliamaarek@Dalias-MacBook-Pro-3.local>
2016-09-30 16:09:40 +0200

    expanding

--- Node.py
+++ Node.py
@@ -1,4 +1,7 @@
 from NodeAbstract import NodeAbstract
+from Maze import Maze
+from Cell import Cell
+
 class Node(NodeAbstract):
 
 	def state(self, stateValue):
@@ -17,53 +20,53 @@ class Node(NodeAbstract):
 		self.cost = costValue
 
 	def expand(self):
-			children = []
-		#forward
-			child = Node()
-			child.state.direction = 0
-			child.operators = "F"
-			newChild(child)
-			adjustLocation(child)
+		children = []
+	#forward
+		child = Node()
+		child.state.direction = self.state.direction
+		child.operators = "F"
+		child.cost = self.cost + 1
+		newChild(child)
+		adjustLocation(child)		
+		if child.state.direction is not None:
 			children.append(child)
-		#east 
-			child = Node()
-			child.state.direction = 1
-			child.operators = "RR".append("F")
-			newChild(child)
-			adjustLocation(child)
-			children.append(child)
-		#south
-			child = Node()
-			child.state.direction = 2
-			child.operators = "RR".append("RR").append("F")
-			newChild(child)
-			adjustLocation(child)
-			children.append(child)
-		#west
-			child = Node()
-			child.state.direction = 3
-			child.operators = "RL".append("F")
-			newChild(child)
-			adjustLocation(child)
-			children.append(child)
+	#right 
+		child = Node()
+		child.state.direction = (self.state.direction + 1)%4
+		child.operators = "R"
+		child.cost = self.cost 
+		child.state.cell = self.cell
+		newChild(child)
+		children.append(child)
+	#left
+		child = Node()
+		child.state.direction = (self.state.direction +3)%4 
+		child.cost = self.cost 
+		child.operators = "L"
+		child.state.cell = self.cell
+		newChild(child)
+		children.append(child)
 		
 	def adjustLocation(self, child):
-		finalDirection = (self.state.direction + child.state.direction)%3
-		if finalDirection == 0:
-			child.state.cell = self.state.matrix[self.state.cell.row][self.state.cell.column+1]
-		elif finalDirection == 1:
+		finalDirection = (self.state.direction + child.state.direction)%4
+		rows = self.state.matrix.rows
+		columns = self.state.matrix.columns
+		if (finalDirection == 0) and (self.state.cell.column > 0) and ("UP" not in self.state.cell.walls):
+			child.state.cell = self.state.matrix[self.state.cell.row][self.state.cell.column-1]
+		elif (finalDirection == 1) and (self.state.cell.row + 1 < rows) and ("RIGHT" not in self.state.cell.walls):
 			child.state.cell = self.state.matrix[self.state.cell.row+1][self.state.cell.column]
-		elif finalDirection == 2:
-			child.state.cell = self.state.matrix [self.state.cell.row][self.state.cell.column-1]
-		elif finalDirection == 3:
+		elif (finalDirection == 2) and (self.state.cell.column + 1 < columns) and ("DOWN" not in self.state.cell.walls):
+			child.state.cell = self.state.matrix [self.state.cell.row][self.state.cell.column+1]
+		elif (finalDirection == 3) and (self.state.cell.row > 0 )and ("LEFT" not in self.state.cell.walls):
 			child.state.cell = self.state.matrix[self.state.cell.row-1][self.state.cell.column]
+		else:
+			finalDirection = None
 		child.state.direction = finalDirection
 
 	def newChild(self, child):
-			child.depth = self.depth + 1
-			child.cost = self.cost + 1
-			child.parent = self
-			child.state.matrix = self.state.matrix
+		child.depth = self.depth + 1
+		child.parent = self
+		child.state.matrix = self.state.matrix
 
 n = Node()
 # n.expand()
