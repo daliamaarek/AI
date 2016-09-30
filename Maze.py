fbd5d43f979ac2a6b4f3e6dac1805aeda023dc25
ahmad-diaa <ahmaddiaa93@gmail.com>
2016-09-30 17:25:36 +0300

    randomly generated steps + randomly assign pokemons to cells

--- Maze.py
+++ Maze.py
@@ -1,5 +1,5 @@
-from random import randint
-from Cell import Cell
+from random import randint, random, sample
+from Cell import *
 
 class Maze:
 	
@@ -37,9 +37,38 @@ class Maze:
 				self.field[start[0]][start[1]].walls.remove("RIGHT")
 			if("LEFT" in self.field[end[0]][end[1]].walls):
 				self.field[end[0]][end[1]].walls.remove("LEFT")
+				
+	def updateRowColumn(self,curr_wall, new_row, new_column):
+		if(curr_wall[2] == "DOWN"):
+				new_row += 1
+		elif(curr_wall[2] == "UP"):
+			new_row -= 1 
+		elif(curr_wall[2] == "LEFT"):
+			new_column -= 1
+		else:
+			new_column += 1
+		return (new_row,new_column)
+	
+	def genMaze(self):
+		self.primsAlgorithm()
+		rows = self.rows
+		columns = self.columns
+		min_steps = int(rows*columns*(1.0/3))
+		self.steps = randint(min_steps, rows*columns*2)
+		self.no_of_pokemons = randint(1,int(rows*columns*(1.0/4)))
+		self.pokemons = sample(range(0, rows*columns - 1), self.no_of_pokemons)
+		# print self.steps
+		# print self.pokemons
+		for i in range(0,len(self.pokemons)):
+			curr_pos = self.pokemons[i]
+			row = curr_pos/columns
+			column = curr_pos - row*columns
+			# print str(row) + " " + str(column)
+			self.field[row][column].isPokemon = True
+
 
 	# Randomized Prim's algorithm
-	def genMaze(self):
+	def primsAlgorithm(self):
 		start_row = randint(0,self.rows - 1)
 		start_column = randint(0,self.columns - 1)
 		all_walls = []
@@ -59,17 +88,11 @@ class Maze:
 			if((curr_wall[0] == 0 and curr_wall[2] == "UP") or (curr_wall[1] == 0 and curr_wall[2] == "LEFT")
 				or (curr_wall[0] == self.rows - 1 and curr_wall[2] == "DOWN") or (curr_wall[1] == self.columns - 1 and curr_wall[2] == "RIGHT")):
 				continue
+			
 			new_row = curr_wall[0]
 			new_column = curr_wall[1]
 
-			if(curr_wall[2] == "DOWN"):
-				new_row += 1
-			elif(curr_wall[2] == "UP"):
-				new_row -= 1 
-			elif(curr_wall[2] == "LEFT"):
-				new_column -= 1
-			else:
-				new_column += 1
+			
 
 			
 			# print str(curr_wall[0]) + " " + str(curr_wall[1])
