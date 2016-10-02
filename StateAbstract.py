f041d4d3ae8c5fb658c148ba6705a055b2f71834
Dalia Maarek <daliamaarek@Dalias-MacBook-Pro-3.local>
2016-10-02 18:27:59 +0200

    adjusted stateAbstract

--- StateAbstract.py
+++ StateAbstract.py
@@ -1,20 +1,6 @@
-
-
 class State():
 
 	self.row = 0
 	self.column = 0
 	self.direction = 0
 	self.pokemonCaptured = ""
-
-	def setRow(self, row):
-		self.row = row
-
-	def setColumn(self, column):
-		self.column = column 
-
-	def setDirection(self, direction):
-		self.direction = direction
-		
-	def setPokemonCaptured(self, pokemonCaptured):
-		self.pokemonCaptured = pokemonCaptured
